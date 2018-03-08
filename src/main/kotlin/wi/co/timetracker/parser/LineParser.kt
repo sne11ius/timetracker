1f412abec964d18b2000938023d2a07438bdcf28
Cornelius Wichering <cornelius.lilge@gmail.com>
2018-03-09 00:21:06 +0100

    Refactoring

--- src/main/kotlin/wi/co/timetracker/parser/LineParser.kt
+++ src/main/kotlin/wi/co/timetracker/parser/LineParser.kt
@@ -11,9 +11,6 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.Collections.emptyList
 
-
-data class SingleLineParseResult(val errors: List<ParseError>, val entry: EntryModel?)
-
 const val MAX_TEXT_LENGTH = 200
 const val MAX_COMMENT_LENGTH = 200
 const val MAX_SUM_HOURS = 2
@@ -41,200 +38,243 @@ class LineParser {
         SUM_MINUTE_PART
     }
 
-    fun parseLine(baseDate: LocalDate, line: String, travelIndicators: List<String>, travelMultiplier: Float): SingleLineParseResult {
-        var state = State.HOURS_START
-        var hoursStart = ""
-        var minutesStart = ""
-        var skipToEnd = ""
-        var hoursEnd = ""
-        var minutesEnd = ""
-        var skipToText = ""
-        var text = ""
-        var comment = ""
-        var skipToSumFromComment = ""
-        var sumHours = ""
-        var sumMinutePart = ""
+    data class SingleLineParseResult(val errors: List<ParseError>, val entry: EntryModel?)
 
-        log.debug { "Parse line $line" }
+    private sealed class ParseResult {
+        data class ParserState(
+                val state: State = State.HOURS_START,
+                val hoursStart: String = "",
+                val minutesStart: String = "",
+                val skipToEnd: String = "",
+                val hoursEnd: String = "",
+                val minutesEnd: String = "",
+                val skipToText: String = "",
+                val text: String = "",
+                val comment: String = "",
+                val skipToSumFromComment: String = "",
+                val sumHours: String = "",
+                val sumMinutePart: String = ""
+        ): ParseResult()
+        data class ParseError(
+                val charIndex: Int,
+                val msg: String
+        ): ParseResult()
+    }
 
-        for (i in 0 until line.length) {
-            val char = line[i]
-            log.debug { "Parse '$char' at $i" }
-            when (state) {
-                State.HOURS_START -> if (hoursStart.isEmpty()) {
+    private fun cannotError(state: State, charIndex: Int, char: Char)
+            = ParseResult.ParseError(charIndex, "This should not happen at state $state: char $char at index is $charIndex")
+
+    private fun charError(charIndex: Int, expectedChar: String, actualChar: Char)
+            = ParseResult.ParseError(charIndex, "Unexpected character '$actualChar'. Expected $expectedChar.")
+
+    private val stateHandlers: Map<State, (Char, Int, ParseResult.ParserState) -> ParseResult> = mapOf(
+            State.HOURS_START to { char, charIndex, state ->
+                val hoursStart = state.hoursStart + char
+                if (state.hoursStart.isEmpty()) {
                     if (char in '0'..'2') {
-                        hoursStart += char
-                        if (hoursStart.length == 2) {
-                            state = State.SKIP_TO_MINUTES_START
-                        }
+                        state.copy(hoursStart = hoursStart)
                     } else
-                        return err(i, "Unexpected character '$char'. Expected 0..2.")
-                } else if (1 == hoursStart.length) {
-                    if (hoursStart.startsWith("0") || hoursStart.startsWith("1")) {
+                        charError(charIndex, "0..2", char)
+                } else if (1 == state.hoursStart.length) {
+                    if (state.hoursStart.startsWith("0") || state.hoursStart.startsWith("1")) {
                         if (char in '0'..'9') {
-                            hoursStart += char
-                            state = State.SKIP_TO_MINUTES_START
+                            state.copy(hoursStart = hoursStart, state = State.SKIP_TO_MINUTES_START)
                         } else
-                            return err(i, "Unexpected character '$char'. Expected 0..9.")
-                    } else if (hoursStart.startsWith("2")) {
+                            charError(charIndex, "0..9", char)
+                    } else if (state.hoursStart.startsWith("2")) {
                         if (char in '0'..'3') {
-                            hoursStart += char
-                            state = State.SKIP_TO_MINUTES_START
+                            state.copy(hoursStart = hoursStart, state = State.SKIP_TO_MINUTES_START)
                         } else
-                            return err(i, "Unexpected character '$char'. Expected 0..3.")
+                            charError(charIndex, "0..3", char)
+                    } else {
+                        cannotError(state.state, charIndex, char)
                     }
                 } else
-                    return err(i, "Unexpected character '$char'. Expected 0..9.")
-                State.SKIP_TO_MINUTES_START -> if (char == ':') {
-                    state = State.MINUTES_START
+                    charError(charIndex, "0..9", char)
+            },
+            State.SKIP_TO_MINUTES_START to { char, charIndex, state ->
+                if (char == ':') {
+                    state.copy(state = State.MINUTES_START)
                 } else {
-                    return err(i, "Unexpected character '$char'. Expected :.")
+                    charError(charIndex, ":", char)
                 }
-                State.MINUTES_START -> if (char in '0'..'9') {
-                    minutesStart += char
+            },
+            State.MINUTES_START to { char, charIndex, state ->
+                if (char in '0'..'9') {
+                    val minutesStart = state.minutesStart + char
                     if (minutesStart.length == 2) {
-                        state = State.SKIP_TO_HOURS_END
+                        state.copy(minutesStart = minutesStart, state = State.SKIP_TO_HOURS_END)
+                    } else {
+                        state.copy(minutesStart = minutesStart)
                     }
                 } else
-                    return err(i, "Unexpected character '$char'. Expected 0..9.")
-                State.SKIP_TO_HOURS_END -> if (skipToEnd == "") {
+                    charError(charIndex, "0..9", char)
+            },
+            State.SKIP_TO_HOURS_END to { char, charIndex, state ->
+                if (state.skipToEnd == "") {
                     if (char == ' ') {
-                        skipToEnd = " "
+                        state.copy(skipToEnd = " ")
                     } else {
-                        return err(i, "Unexpected character '$char'. Expected ' '.")
+                        charError(charIndex, "' '", char)
                     }
-                } else if (skipToEnd == " ") {
+                } else if (state.skipToEnd == " ") {
                     if (char == '-') {
-                        skipToEnd = " -"
+                        state.copy(skipToEnd = " -")
                     } else {
-                        return err(i, "Unexpected character '$char'. Expected '-'.")
+                        charError(charIndex, "-", char)
                     }
-                } else if (skipToEnd == " -") {
+                } else if (state.skipToEnd == " -") {
                     if (char == ' ') {
-                        skipToEnd = " - "
-                        state = State.HOURS_END
+                        state.copy(skipToEnd = " - ", state = State.HOURS_END)
                     } else {
-                        return err(i, "Unexpected character '$char'. Expected ' '.")
+                        charError(charIndex, "' '", char)
                     }
+                } else {
+                    cannotError(state.state, charIndex, char)
                 }
-                State.HOURS_END -> if (hoursEnd.isEmpty()) {
+            },
+            State.HOURS_END to { char, charIndex, state ->
+                val hoursEnd = state.hoursEnd + char
+                if (state.hoursEnd.isEmpty()) {
                     if (char in '0'..'2') {
-                        hoursEnd += char
-                        if (hoursEnd.length == 2) {
-                            state = State.SKIP_TO_MINUTES_END
-                        }
+                        state.copy(hoursEnd = hoursEnd)
                     } else
-                        return err(i, "Unexpected character '$char'. Expected 0..2.")
-                } else if (1 == hoursEnd.length) {
+                        charError(charIndex, "0..2", char)
+                } else if (1 == state.hoursEnd.length) {
                     if (hoursEnd.startsWith("0") || hoursEnd.startsWith("1")) {
                         if (char in '0'..'9') {
-                            hoursEnd += char
-                            state = State.SKIP_TO_MINUTES_END
+                            state.copy(hoursEnd = hoursEnd, state = State.SKIP_TO_MINUTES_END)
                         } else
-                            return err(i, "Unexpected character '$char'. Expected 0..9.")
+                            charError(charIndex, "0..9", char)
                     } else if (hoursEnd.startsWith("2")) {
                         if (char in '0'..'3') {
-                            hoursEnd += char
-                            state = State.SKIP_TO_MINUTES_END
+                            state.copy(hoursEnd = hoursEnd, state = State.SKIP_TO_MINUTES_END)
                         } else
-                            return err(i, "Unexpected character '$char'. Expected 0..3.")
+                            charError(charIndex, "0..3", char)
+                    } else {
+                        cannotError(state.state, charIndex, char)
                     }
                 } else
-                    return err(i, "Unexpected character '$char'. Expected 0..9.")
-                State.SKIP_TO_MINUTES_END -> {
-                    if (char == ':') {
-                        state = State.MINUTES_END
-                    } else {
-                        return err(i, "Unexpected character '$char'. Expected :.")
-                    }
-                }
-                State.MINUTES_END -> if (char in '0'..'9') {
-                    minutesEnd += char
-                    if (minutesEnd.length == 2) {
-                        state = State.SKIP_TO_TEXT
-                    }
-                } else
-                    return err(i, "Unexpected character '$char'. Expected 0..9.")
-                State.SKIP_TO_TEXT -> if (skipToText == "") {
-                    skipToText = if (char == ':')
-                        ":"
-                    else
-                        return err(i, "Unexpected character '$char'. Expected ' '.")
-                } else if (skipToText == ":") {
-                    if (char == ' ') {
-                        skipToText = ": "
-                        state = State.TEXT
-                    } else {
-                        return err(i, "Unexpected character '$char'. Expected '-'.")
-                    }
-                }
-                State.TEXT -> when {
-                    i > MAX_TEXT_LENGTH -> return err(i, "Max text length of $MAX_TEXT_LENGTH exceeded.")
-                    char == ',' ->
-                        return err(i, "Unexpected character '$char'. Expected not ','.")
-                    char == '(' -> state = State.COMMENT
-                    char == '=' -> state = State.SKIP_TO_SUM_FROM_TEXT
-                    else -> text += char
-                }
-                State.COMMENT -> when {
-                    i > MAX_COMMENT_LENGTH ->
-                        return err(i, "Max comment length of $MAX_COMMENT_LENGTH exceeded.")
-                    char == ')' -> state = State.SKIP_TO_SUM_FROM_COMMENT
-                    else -> comment += char
-                }
-                State.SKIP_TO_SUM_FROM_TEXT -> if (char == ' ') {
-                    state = State.SUM_HOURS
+                    charError(charIndex, "0..9", char)
+            },
+            State.SKIP_TO_MINUTES_END to { char, charIndex, state ->
+                if (char == ':') {
+                    state.copy(state = State.MINUTES_END)
                 } else {
-                    return err(i, "Unexpected character '$char'. Expected ' '.")
+                    charError(charIndex, ":", char)
                 }
-                State.SKIP_TO_SUM_FROM_COMMENT -> {
-                    if (skipToSumFromComment == "") {
-                        if (char == ' ') {
-                            skipToSumFromComment = " "
-                        } else
-                            return err(i, "Unexpected character '$char'. Expected ' '.")
-                    } else if (skipToSumFromComment == " ") {
-                        if (char == '=') {
-                            skipToSumFromComment = " ="
-                        } else
-                            return err(i, "Unexpected character '$char'. Expected '='.")
-                    } else if (skipToSumFromComment == " =") {
-                        if (char == ' ') {
-                            state = State.SUM_HOURS
-                        } else
-                            return err(i, "Unexpected character '$char'. Expected '='.")
+            },
+            State.MINUTES_END to { char, charIndex, state ->
+                val minutesEnd = state.minutesEnd + char
+                if (char in '0'..'9') {
+                    if (minutesEnd.length == 2) {
+                        state.copy(minutesEnd = minutesEnd, state = State.SKIP_TO_TEXT)
+                    } else {
+                        state.copy(minutesEnd = minutesEnd)
                     }
+                } else
+                    charError(charIndex, "0..9", char)
+            },
+            State.SKIP_TO_TEXT to { char, charIndex, state ->
+                if (state.skipToText == "") {
+                    if (char == ':')
+                        state.copy(skipToText = ":")
+                    else
+                        charError(charIndex, "' '", char)
+                } else if (state.skipToText == ":") {
+                    if (char == ' ') {
+                        state.copy(skipToText = ": ", state = State.TEXT)
+                    } else {
+                    charError(charIndex, "'-'", char)
+                    }
+                } else {
+                    cannotError(state.state, charIndex, char)
                 }
-
-                State.SUM_HOURS -> if (sumHours.length > MAX_SUM_HOURS)
-                    return err(i, "Max hour part length exceeded.")
+            },
+            State.TEXT to { char, charIndex, state -> when {
+                    charIndex > MAX_TEXT_LENGTH -> ParseResult.ParseError(charIndex, "Max text length of $MAX_TEXT_LENGTH exceeded.")
+                    char == ',' -> charError(charIndex, "not ','", char)
+                    char == '(' -> state.copy(state = State.COMMENT)
+                    char == '=' -> state.copy(state = State.SKIP_TO_SUM_FROM_TEXT)
+                    else -> state.copy(text = state.text + char)
+                }
+            },
+            State.COMMENT to { char, charIndex, state -> when {
+                    charIndex > MAX_COMMENT_LENGTH -> ParseResult.ParseError(charIndex, "Max comment length of $MAX_COMMENT_LENGTH exceeded.")
+                    char == ')' -> state.copy(state = State.SKIP_TO_SUM_FROM_COMMENT)
+                    else -> state.copy(comment = state.comment + char)
+                }
+            },
+            State.SKIP_TO_SUM_FROM_TEXT to { char, charIndex, state ->
+                if (char == ' ') {
+                    state.copy(state = State.SUM_HOURS)
+                } else {
+                    charError(charIndex, "' '", char)
+                }
+            },
+            State.SKIP_TO_SUM_FROM_COMMENT to { char, charIndex, state ->
+                if (state.skipToSumFromComment == "") {
+                    if (char == ' ') {
+                        state.copy(skipToSumFromComment = " ")
+                    } else
+                        charError(charIndex, "' '", char)
+                } else if (state.skipToSumFromComment == " ") {
+                    if (char == '=') {
+                        state.copy(skipToSumFromComment = " ")
+                    }
+                    charError(charIndex, "=", char)
+                } else if (state.skipToSumFromComment == " =") {
+                    if (char == ' ') {
+                        state.copy(state = State.SUM_HOURS)
+                    } else
+                        charError(charIndex, "=", char)
+                } else {
+                    cannotError(state.state, charIndex, char)
+                }
+            },
+            State.SUM_HOURS to { char, charIndex, state ->
+                if (state.sumHours.length > MAX_SUM_HOURS)
+                    ParseResult.ParseError(charIndex, "Max hour part length exceeded.")
                 else when (char) {
-                    in '0'..'9' -> sumHours += char
-                    ',' -> state = State.SUM_MINUTE_PART
-                    else ->
-                        return err(i, "Unexpected character '$char'. Expected ',' or '0'..'9'.")
+                    in '0'..'9' -> state.copy(sumHours = state.sumHours + char)
+                    ',' -> state.copy(state = State.SUM_MINUTE_PART)
+                    else ->charError(charIndex, "',' or '0'..'9'", char)
                 }
-                State.SUM_MINUTE_PART -> sumMinutePart += when {
-                    sumMinutePart.length > MAX_SUM_MINUTE_PART -> return err(i, "Max minute part length exceeded.")
-                    char in '0'..'9' -> char
-                    else ->
-                        return err(i, "Unexpected character '$char'. Expected '0'..'9'.")
+            },
+            State.SUM_MINUTE_PART to { char, charIndex, state ->
+                when {
+                    state.sumMinutePart.length > MAX_SUM_MINUTE_PART -> ParseResult.ParseError(charIndex, "Max minute part length exceeded.")
+                    char in '0'..'9' -> state.copy(sumMinutePart = state.sumMinutePart + char)
+                    else ->charError(charIndex, "0..9", char)
+                }
+            }
+    )
+
+    fun parseLine(baseDate: LocalDate, line: String, travelIndicators: List<String>, travelMultiplier: Float): SingleLineParseResult {
+        var state: ParseResult = ParseResult.ParserState()
+        log.debug { "Parse line $line" }
+        for (i in 0 until line.length) {
+            val char = line[i]
+            state = when (state) {
+                is ParseResult.ParserState -> stateHandlers[state.state]!!.invoke(char, i, state)
+                is ParseResult.ParseError -> {
+                    return err(state.charIndex, state.msg)
                 }
             }
         }
 
-        when (state) {
+        when ((state as ParseResult.ParserState).state) {
             State.TEXT, State.SKIP_TO_SUM_FROM_TEXT, State.SKIP_TO_SUM_FROM_COMMENT, State.SUM_HOURS, State.SUM_MINUTE_PART -> {
                 return mkEntry(
                         baseDate,
-                        hoursStart,
-                        minutesStart,
-                        hoursEnd,
-                        minutesEnd,
-                        text,
-                        comment,
-                        sumHours,
-                        sumMinutePart,
+                        state.hoursStart,
+                        state.minutesStart,
+                        state.hoursEnd,
+                        state.minutesEnd,
+                        state.text,
+                        state.comment,
+                        state.sumHours,
+                        state.sumMinutePart,
                         travelIndicators,
                         travelMultiplier
                 )
