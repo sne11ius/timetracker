02f308c1a7f0df147717b0ee5773db55da772a23
Cornelius Wichering <cornelius.wichering@engram.de>
2019-01-18 21:06:27 +0100

    Auto formatted code

--- src/main/kotlin/wi/co/timetracker/parser/LineParser.kt
+++ src/main/kotlin/wi/co/timetracker/parser/LineParser.kt
@@ -17,42 +17,37 @@ import java.time.temporal.ChronoUnit.MINUTES
 @Component
 class LineParser : Grammar<EntryModel>() {
 
-    data class SingleLineParseResult(val errors: List<ParseError>, val entry: EntryModel? = null)
+  data class SingleLineParseResult(val errors: List<ParseError>, val entry: EntryModel? = null)
 
-    private val hours by token("[01][0-9]|2[0-3]")
-    private val minutes by token("[0-5][0-9]")
-    private val sep by token(":")
-    private val fromToSep by token(" - ")
-    private val space by token(" ")
-    private val lparen by token("\\(", ignore = true)
-    private val rparen by token("\\)", ignore = true)
-    private val text by token("[^()]+")
+  private val hours by token("[01][0-9]|2[0-3]")
+  private val minutes by token("[0-5][0-9]")
+  private val sep by token(":")
+  private val fromToSep by token(" - ")
+  private val space by token(" ")
+  private val lparen by token("\\(", ignore = true)
+  private val rparen by token("\\)", ignore = true)
+  private val text by token("[^()]+")
 
-    private val hoursP = hours use { text.toInt() }
-    private val minutesP by (hours or minutes) use { text.toInt() }
-    private val textP = text use { text.trim() }
+  private val hoursP = hours use { text.toInt() }
+  private val minutesP by (hours or minutes) use { text.toInt() }
+  private val textP = text use { text.trim() }
 
-    val time
-            = hoursP and sep and minutesP
-    private val timeP = time map { (h, _, m) -> LocalDateTime.now().truncatedTo(MINUTES).withHour(h).withMinute(m) }
-    private val fromTo
-            = timeP and fromToSep and timeP
-    private val fromToP = fromTo map { (begin, _, end) -> Pair(begin, end) }
+  val time = hoursP and sep and minutesP
+  private val timeP = time map { (h, _, m) -> LocalDateTime.now().truncatedTo(MINUTES).withHour(h).withMinute(m) }
+  private val fromTo = timeP and fromToSep and timeP
+  private val fromToP = fromTo map { (begin, _, end) -> Pair(begin, end) }
 
-    private val entryP
-            = fromToP and sep and space and textP map { (ft, _, _, t) -> Pair(ft, t) }
+  private val entryP = fromToP and sep and space and textP map { (ft, _, _, t) -> Pair(ft, t) }
 
-    private val commentP
-            = -lparen and textP and -rparen
+  private val commentP = -lparen and textP and -rparen
 
-    private val entryWithComment
-            = entryP and optional(commentP) map { (entry, comment) ->
-        val (begin, end) = entry.first
-        val text = entry.second
-        EntryModel(begin, end, text.trim(), comment.orEmpty().trim())
-    }
+  private val entryWithComment = entryP and optional(commentP) map { (entry, comment) ->
+    val (begin, end) = entry.first
+    val text = entry.second
+    EntryModel(begin, end, text.trim(), comment.orEmpty().trim())
+  }
 
-    override val rootParser = entryWithComment
+  override val rootParser = entryWithComment
 
     fun parseLine(baseDate: LocalDate, line: String): SingleLineParseResult {
         val res = tryParseToEnd(line)
